f9d4c75275d965bbc74288c5d361c313f55aabf1
salihabrik <briksaliha@gmail.com>
2023-10-01 16:22:06 +0200

    first commit

---
+++ project.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x[5] = { 22, 18, 2, 55, 520 };
+
+	cout << X[0] << endl;
+	cout << X[1] << endl;
+	cout << X[2] << endl;
+	cout << X[3] << endl;
+	cout << X[4] << endl;
+
+	return 0;
+}
